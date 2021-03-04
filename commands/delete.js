5a2f1a56607108ff87f10900483ff64bdde9d40a
agusnarestha <63548030+agusnarestha@users.noreply.github.com>
2021-03-04 14:50:46 +0800

    revisi delete.js

--- commands/delete.js
+++ commands/delete.js
@@ -5,14 +5,14 @@ module.exports ={
         const amount = parseInt(args[0]) + 1;
 
         if (isNaN(amount)) {
-            return message.channel.send('Please Enter A Number.')
+            return message.channel.send('Masukkan nilai')
         } else if (amount <= 1 || amount > 100) {
-            return message.channel.send('You Can Only Delete Messages From 1 To 99.')
+            return message.channel.send('Kamu hanya bisa menghapus dari 1 sampai 99.')
         }
 
         message.channel.bulkDelete(amount, true).catch(err => {
             console.error(err);
-            message.channel.send('There Was An Error Deleting Messages In This Cahnnel.')
+            message.channel.send('Terjadi error pada saat menghapus pesan.')
         })
     }
 }
