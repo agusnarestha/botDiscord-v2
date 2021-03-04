1c4c422e8d428c9ba7886897136944d7f2ff61e8
agusnarestha <63548030+agusnarestha@users.noreply.github.com>
2021-03-04 20:36:42 +0800

    update welcome.js

--- commands/welcome.js
+++ commands/welcome.js
@@ -13,15 +13,4 @@ module.exports = (client) => {
           const channel = member.guild.channels.cache.get(welcomechannelId)
           channel.send(welocmemessage)
       })
-      
-      // Leave Message Command
-  
-      const leavechannelId = '' //Channel You Want to Send The Leave Message
-  
-      client.on('guildMemberRemove', (member) => {
-          const leavemessage = `<@${member.id}> Just Left Server.`
-  
-          const channel1 = member.guild.channels.cache.get(leavechannelId)
-          channel1.send(leavemessage)
-      })
   }
