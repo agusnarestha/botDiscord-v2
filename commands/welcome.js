ee62eb1574c6415bd8b4d3100ee3b4e980ca42da
agusnarestha <63548030+agusnarestha@users.noreply.github.com>
2021-03-08 21:31:25 +0800

    updateing welcome.js

--- commands/welcome.js
+++ commands/welcome.js
@@ -1,16 +1,16 @@
 module.exports = (client) => {
 
     // Welcome Message Command
-      const welcomechannelId = '' //Channel You Want to Send The Welcome Message
-      const targetChannelId = `` //Channel For Rules
+      const welcomechannelId = '700697889743306824' //Channel ID Welcome
+      const targetChannelId = `795465232951541800` //Channel ID Rules and Roles
   
       client.on('guildMemberAdd', (member) => {
           console.log(member)
           
-          const welocmemessage = ` <@${member.id}> Welcome To Our Server,
+          const welcomemessage = ` <@${member.id}> Welcome To Our Server,
   Please Read ${member.guild.channels.cache.get(targetChannelId).toString()}
   Have A Nice Time!`
           const channel = member.guild.channels.cache.get(welcomechannelId)
-          channel.send(welocmemessage)
+          channel.send(welcomemessage)
       })
   }
