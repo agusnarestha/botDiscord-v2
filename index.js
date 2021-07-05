88662cbdc5711af6c6b9aa6c64412b3048d043a8
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2021-07-05 15:27:59 +0700

    command handling dan lain lain

--- index.js
+++ index.js
@@ -1,48 +1,85 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.commands = new Discord.Collection();
 const fs = require('fs');
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`)
-    client.commands.set(command.name, command) 
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+client.cooldowns = new Discord.Collection();
+
+const commandFolders = fs.readdirSync('./commands');
+
+for (const folder of commandFolders) {
+	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const command = require(`./commands/${folder}/${file}`);
+		client.commands.set(command.name, command);
+	}
 }
 
-const { prefix, token } = require('./config.json')
-
 client.once('ready', () => {
-    console.log('Bot Berjalan...')
-})
-    
-    //Below Auto-Changing Status
-    setInterval(() => {
-        const statuses = [
-            `Patuhi Protokol Kesehatan`,
-            `Jaga Jarak`,
-            `Pakai Masker`,
-        ]
-
-        const status = statuses[Math.floor(Math.random() * statuses.length)]
-        client.user.setActivity(status, { type: "WATCHING"}) 
-    }, 2000)
+	console.log('Bot Berjalan!');
+});
 
 client.on('message', message => {
-    if(!message.content.startsWith(prefix)||message.author.bot) return
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/)
-    const command = args.shift().toLowerCase()
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
 
-    if(command === 'ping'){
-        client.commands.get('ping').execute(message, args)
-    } else if(command === 'badut'){
-        client.commands.get('badut').execute(message, args)
-    } else if(command === 'infoserver'){
-        client.commands.get('infoserver').execute(message, args)
-    } else if(command === 'infoserver'){
-        client.commands.get('infoserver').execute(message, args)
-    } 
-})
+	const command = client.commands.get(commandName)
+		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
+	if (!command) return;
 
-client.login(token)
+	if (command.guildOnly && message.channel.type === 'dm') {
+		return message.reply('I can\'t execute that command inside DMs!');
+	}
+
+	if (command.permissions) {
+		const authorPerms = message.channel.permissionsFor(message.author);
+		if (!authorPerms || !authorPerms.has(command.permissions)) {
+			return message.reply('You can not do this!');
+		}
+	}
+
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
+
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+		}
+
+		return message.channel.send(reply);
+	}
+
+	const { cooldowns } = client;
+
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+	try {
+		command.execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
+client.login(token);
